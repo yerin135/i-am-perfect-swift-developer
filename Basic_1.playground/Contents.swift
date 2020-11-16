a55a1283ca40e16b8e7a3f3feaff97c86a18259c
yerin jeon <rin9682@daum.net>
2020-11-17 00:22:08 +0900

    음...

---
+++ Basic_1.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+//변수 상수
+
+var firstName = "jeon"
+firstName = "aa"
+firstName = "aa"
+
+print(firstName)
+
+//상수 : 값 변경 X
+
+let myID = "abcd"
+//myID = "a"
+
+print(myID)
+
