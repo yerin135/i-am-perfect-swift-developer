4f41c437064cb7b06cb49ade3afcb8cbb6214ebf
yerin jeon <rin9682@daum.net>
2020-11-17 23:35:39 +0900

    흠....

---
+++ Basic_2.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//Type
+
+//Type - String
+var myName = "jeonyerin" //string
+var firstName: String = "jeon"
+
+//Type - Int, Double, Float
+var myID: Double = 50
+var myUUID: Float = 100.0
+
+
+var lastName: String = "abc"
+
